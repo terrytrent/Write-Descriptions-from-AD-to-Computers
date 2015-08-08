9df9961c8a4b76814f1e1e3590431b2b4244ccf5
Terry Trent <ttrent1101@gmail.com>
2015-08-08 16:19:30 -0400

    On branch master  Your branch is up-to-date with 'origin/master'.
     Changes to be committed:
    	new file:   write-description-to-computer.ps1

---
+++ write-description-to-computer.ps1
@@ -0,0 +1,27 @@
+﻿$allServers=get-adcomputer -Properties * -Filter * | where {$_.DistinguishedName -like "*,OU=Servers,DC=domain,DC=com"}
+
+foreach($server in $allServers){
+    
+    $psVersion=$null
+    $serverName=$server.CN
+    $description=$server.description
+
+    write-host "The Description for $serverName is $description"
+
+    $psVersion=(invoke-command -ComputerName $serverName -ScriptBlock {$PSVersionTable.PSVersion.Major} -ErrorAction SilentlyContinue)
+
+    if($psVersion -eq $null){
+
+        C:\pstools\psexec.exe -u domain\user -p Password \\$serverName net config server /srvcomment:"$description"
+
+    }
+    else{
+
+        $fullProperties=get-wmiobject -class Win32_OperatingSystem -computername "$serverName"
+        $fullProperties.Description = "$description"
+        $fullProperties.Put()
+    
+    }
+    
+}
+
